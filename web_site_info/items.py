fc101988041be69f465394ae65e6963920bb8015
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2020-11-15 10:41:19 -0300

    feature: Extract the websites’ logo image URLs

--- web_site_info/items.py
+++ web_site_info/items.py
@@ -4,9 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from itemloaders.processors import TakeFirst, MapCompose
+from scrapy.loader import ItemLoader
 
 class WebSiteInfoItem(scrapy.Item):
     logo = scrapy.Field()
     phone = scrapy.Field()
     website = scrapy.Field()
+
+
+class WebSiteInfoItemLoader(ItemLoader):
+
+    default_input_processor = TakeFirst()
+    default_output_processor = TakeFirst()
--- web_site_info/spiders/site_info.py
+++ web_site_info/spiders/site_info.py
@@ -1,10 +1,15 @@
 import scrapy
+from web_site_info.items import WebSiteInfoItem, WebSiteInfoItemLoader
 
+IMG_LOGO_XPATH = '//img[contains(@src, "logo")]/@src'
 
 class SiteInfoSpider(scrapy.Spider):
     name = 'site_info'
-    allowed_domains = ['']
-    start_urls = ['http:///']
+    start_urls = ['https://www.phosagro.com/']
 
     def parse(self, response):
-        pass
+        site_info = WebSiteInfoItemLoader(item=WebSiteInfoItem(), response=response)
+        path_image = response.urljoin(response.xpath(IMG_LOGO_XPATH).extract_first())
+
+        site_info.add_value('logo', path_image)
+        return site_info.load_item()
