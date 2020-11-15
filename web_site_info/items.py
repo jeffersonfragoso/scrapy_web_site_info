f1936f9afa6e825825e308b67f95f0a88ed01662
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2020-11-15 14:35:44 -0300

    feature: Extract phone numbers

--- web_site_info/items.py
+++ web_site_info/items.py
@@ -3,17 +3,28 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import re
 import scrapy
-from itemloaders.processors import TakeFirst, MapCompose
+
+from itemloaders.processors import Compose
 from scrapy.loader import ItemLoader
 
 class WebSiteInfoItem(scrapy.Item):
     logo = scrapy.Field()
-    phone = scrapy.Field()
+    phones = scrapy.Field()
     website = scrapy.Field()
 
 
 class WebSiteInfoItemLoader(ItemLoader):
+    def sanitize_phone(phones):
+        """
+            Remove any characters that are not digits, a plus sign (+) or parentheses.
 
-    default_input_processor = TakeFirst()
-    default_output_processor = TakeFirst()
+            Parameters:
+                - phoness (list) - Object whit text phones extracted
+        """
+
+        pattern = r'[+()\s]|[0-9]'
+        return ["".join(re.findall(pattern, phone)) for phone in phones]
+
+    phones_in = Compose(sanitize_phone)
--- web_site_info/spiders/site_info.py
+++ web_site_info/spiders/site_info.py
@@ -1,15 +1,34 @@
+import re
 import scrapy
+
 from web_site_info.items import WebSiteInfoItem, WebSiteInfoItemLoader
 
 IMG_LOGO_XPATH = '//img[contains(@src, "logo")]/@src'
+CONTACT_XPATH = '//a[contains(@href, "contact")]/@href'
 
 class SiteInfoSpider(scrapy.Spider):
     name = 'site_info'
     start_urls = ['https://www.phosagro.com/']
 
     def parse(self, response):
-        site_info = WebSiteInfoItemLoader(item=WebSiteInfoItem(), response=response)
-        path_image = response.urljoin(response.xpath(IMG_LOGO_XPATH).extract_first())
+        page_contact = response.xpath(CONTACT_XPATH).extract_first()
+        yield response.follow(page_contact, self.parse_site_info)
 
-        site_info.add_value('logo', path_image)
+    def parse_site_info(self, response):
+        path_logo = self.extract_logo_path(response)
+        phones = self.extract_phones(response)
+
+        site_info = WebSiteInfoItemLoader(item=WebSiteInfoItem(), response=response)
+        site_info.add_value('logo', path_logo)
+        site_info.add_value('phones', phones)
         return site_info.load_item()
+
+    def extract_logo_path(self, response):
+        logo_path = response.urljoin(response.xpath(IMG_LOGO_XPATH).extract_first())
+        return logo_path
+
+    def extract_phones(self, response):
+        page_text = response.xpath('normalize-space(//body)').extract_first()
+        phones_pattern = r'[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*'
+        phones = list(set(re.findall(phones_pattern, page_text)))
+        return phones
