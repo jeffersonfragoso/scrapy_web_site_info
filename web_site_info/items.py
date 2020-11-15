1854dad3416abf155ba51ede7eed31c5ac6a5084
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2020-11-15 15:24:19 -0300

    fix: Remove 'WebSiteInfoItemLoader'
    because it was causing a mutation in the logo and website output format

--- web_site_info/items.py
+++ web_site_info/items.py
@@ -6,25 +6,8 @@
 import re
 import scrapy
 
-from itemloaders.processors import MapCompose
-from scrapy.loader import ItemLoader
 
 class WebSiteInfoItem(scrapy.Item):
     logo = scrapy.Field()
     phones = scrapy.Field()
     website = scrapy.Field()
-
-
-class WebSiteInfoItemLoader(ItemLoader):
-    def sanitize_phone(phones):
-        """
-            Remove any characters that are not digits, a plus sign (+) or parentheses.
-
-            Parameters:
-                - phoness (list) - Object whit text phones extracted
-        """
-
-        pattern = r'[+()\s]|[0-9]'
-        return ["".join(re.findall(pattern, phone)) for phone in phones]
-
-    phones_in = MapCompose(sanitize_phone)
