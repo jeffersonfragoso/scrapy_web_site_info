0485ff2496814e1f3e9480b703d3fc6ecf4c6795
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2020-11-16 02:02:57 -0300

    feature: Multiprocessing and writing results to standard output as JSON format.
    Processes each website in a separate job using the scrapyscript library

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
-from scrapy.crawler import CrawlerProcess
+from scrapyscript import Job, Processor
 from scrapy.utils.project import get_project_settings
+
+
 from web_site_info.spiders.site_info import SiteInfoSpider
 
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(SiteInfoSpider)
-process.start()
+
+if __name__ == "__main__":
+
+    start_urls = ['https://www.phosagro.com/', 'https://www.cmsenergy.com/']
+    jobs = list()
+
+    for url in start_urls:
+        job = Job(SiteInfoSpider, url=url)
+        jobs.append(job)
+
+    settings = get_project_settings()
+    processor = Processor(settings)
+    data = processor.run(jobs)
+
+    for item in data:
+        print(item['_values'])
